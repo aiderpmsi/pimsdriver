37205c504565e07a0681cfda94504dc5165f69eb
delabre <j.delabre@aider.asso.fr>
2014-04-07 09:50:07 +0200

    Mise en place gui perso

---
+++ pimsdriver/src/main/java/com/github/aiderpmsi/pimsdriver/vaadin/GuiUI.java
@@ -0,0 +1,72 @@
+package com.github.aiderpmsi.pimsdriver.vaadin;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.MenuBar;
+import com.vaadin.ui.MenuBar.Command;
+import com.vaadin.ui.MenuBar.MenuItem;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+@Theme("pimsdriver")
+public class GuiUI extends UI {
+
+	/**
+	 * Generated Serial
+	 */
+	private static final long serialVersionUID = 3109715875916629911L;
+
+	@Override
+	protected void init(VaadinRequest request) {
+		initLayout();
+	}
+	
+	private void initLayout() {
+		
+		// ROOT OF USER INTERFACE IS VERTICAL LAYOUT
+		VerticalLayout layout = new VerticalLayout();
+		setContent(layout);
+		
+		// HEADER
+		Label header = new Label();
+		header.setCaption("PimsDriver");
+		header.setStyleName("header");
+		layout.addComponent(header);
+		
+		// MENUBAR
+		MenuBar menubar = new MenuBar();
+		menubar.setStyleName("mymenubar");
+		MenuItem files = menubar.addItem("Fichiers", null, null);
+		files.addItem("Ajouter Pmsi", null, new AddPmsi());
+		files.addSeparator();
+		files.addItem("Traitements Pmsi", null, null);	
+		layout.addComponent(menubar);
+		
+		
+		// ADD 3 LITTLE LABEL
+		for (int i = 0 ; i < 3 ; i++) {
+			Label label = new Label();
+			label.setCaption("Contenu " + Integer.toString(i) + " ...");
+			layout.addComponent(label);
+		}
+		
+	}
+	
+	private class AddPmsi implements Command {
+
+		@Override
+		public void menuSelected(MenuItem selectedItem) {
+			Window wHelp = new Window("myAPP Help");
+	        wHelp.setHeight("650px");
+	        wHelp.setWidth("650px");
+			Label label = new Label();
+			label.setCaption("Contenu");
+			wHelp.setContent(label);
+	        wHelp.center();
+	        addWindow(wHelp);
+		}
+		
+	}
+}
