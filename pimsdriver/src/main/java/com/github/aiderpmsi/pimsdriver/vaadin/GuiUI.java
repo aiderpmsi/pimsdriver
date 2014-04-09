74212e3e1fc0f532485670a7c74f3ed448d65bb7
delabre <j.delabre@aider.asso.fr>
2014-04-09 12:07:08 +0200

    Début simplification en séparant les classes

--- pimsdriver/src/main/java/com/github/aiderpmsi/pimsdriver/vaadin/GuiUI.java
+++ pimsdriver/src/main/java/com/github/aiderpmsi/pimsdriver/vaadin/GuiUI.java
@@ -1,22 +1,12 @@
 package com.github.aiderpmsi.pimsdriver.vaadin;
 
-import com.github.aiderpmsi.pimsdriver.jaxrs.importpmsi.ImportRsfModel;
 import com.vaadin.annotations.Theme;
-import com.vaadin.data.fieldgroup.BeanFieldGroup;
-import com.vaadin.data.fieldgroup.FieldGroup.CommitEvent;
-import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
-import com.vaadin.data.fieldgroup.FieldGroup.CommitHandler;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.MenuBar;
-import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.MenuBar.Command;
 import com.vaadin.ui.MenuBar.MenuItem;
-import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.Upload;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
@@ -66,9 +56,6 @@ public class GuiUI extends UI {
 	
 	private class AddPmsi implements Command {
 
-		/**
-		 * Generated serial id
-		 */
 		private static final long serialVersionUID = 1315554704783116551L;
 
 		@Override
@@ -76,59 +63,9 @@ public class GuiUI extends UI {
 			// CREATE WINDOW
 			final Window wHelp = new ImportPmsiUpload();
 	        
-	        // DEFINE THE FORM CONTENT
-	        ImportRsfModel model = new ImportRsfModel();
-	        model.setDefaultValues();
-	        final BeanFieldGroup<ImportRsfModel> binder =
-	        		new BeanFieldGroup<ImportRsfModel>(ImportRsfModel.class);
-	        binder.setItemDataSource(model);
-	        binder.addCommitHandler(new Gestioncommit(fup));
-	        
-	        fl.addComponent(binder.buildAndBind("Année", "yearValue"));
-	        fl.addComponent(binder.buildAndBind("Mois", "monthValue"));
-	        fl.addComponent(binder.buildAndBind("Finess", "finessValue"));
-	        
-	        fl.addComponent(new Button("OK", new Button.ClickListener() {
-				
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				public void buttonClick(ClickEvent event) {
-					try {
-						binder.commit();
-					} catch (CommitException e) {
-						new Notification(e.getMessage());
-					}
-					UI.getCurrent().removeWindow(wHelp);
-	            }
-	        }));
-	        
-	        subContent.addComponent(fl);
-	        
 			UI.getCurrent().addWindow(wHelp);
 		}
 		
 	}
 	
-	private class Gestioncommit implements CommitHandler {
-
-		private FileUploader fup;
-		
-		public Gestioncommit(FileUploader fup) {
-			this.fup = fup;
-		}
-		
-		@Override
-		public void preCommit(CommitEvent commitEvent) throws CommitException {
-			// VERIFY THAT EVERYTHING IS GOOG
-			if (fup.getFilename() == null)
-				throw new CommitException("Aucun fichier rsf choisi");
-		}
-
-		@Override
-		public void postCommit(CommitEvent commitEvent) throws CommitException {
-			// DO NOTHING
-		}
-		
-	}
 }
