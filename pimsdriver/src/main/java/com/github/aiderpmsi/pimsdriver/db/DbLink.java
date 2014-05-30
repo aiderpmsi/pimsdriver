3875a69945fd467440d09feb865f7b2a8f53c18d
jean philippe delabre <jeanphilippe.delabre@gmail.com>
2014-05-30 20:44:49 +0200

    Test using 'copy to' from postgresql, but impossible due to necessary index corresponance

--- pimsdriver/src/main/java/com/github/aiderpmsi/pimsdriver/db/DbLink.java
+++ pimsdriver/src/main/java/com/github/aiderpmsi/pimsdriver/db/DbLink.java
@@ -1,5 +1,6 @@
 package com.github.aiderpmsi.pimsdriver.db;
 
+import java.io.Reader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,12 +10,16 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
+import javax.swing.text.Segment;
+
+import org.eclipse.persistence.internal.sessions.remote.ReplaceValueHoldersIterator;
+
 /**
  * Process makeing the calls to database while main process reads pmsi file
  * @author jpc
  *
  */
-public abstract class DbLink implements Callable<Boolean> {
+public abstract class DbLink extends Reader implements Callable<Boolean> {
 
 	/** Root Id in DB */
 	protected long pmel_root;
@@ -36,6 +41,11 @@ public abstract class DbLink implements Callable<Boolean> {
 
 	@Override
 	public Boolean call() throws InterruptedException, SQLException {
+	}
+	
+	@Override
+	public int read(char[] cbuf, int off, int len) throws java.io.IOException {
+		// CREATES A BUFFER
 		for (;;) {
 			Entry entry = queue.poll(1, TimeUnit.SECONDS);
 
@@ -48,14 +58,24 @@ public abstract class DbLink implements Callable<Boolean> {
 				calculateParent(entry);
 				
 				// SETS THE VALUES OF QUERY ARGS
-				ps.setLong(1, pmel_root);
+				StringBuilder content = new StringBuilder();
+				content.append(Long.toString(pmel_root));
+				content.append('|');
+				
 				Long pmel_parent;
 				if ((pmel_parent = getParent()) == null)
-					ps.setNull(2, Types.BIGINT);
+					content.append("\\N");
 				else
-					ps.setLong(2, pmel_parent);
-				ps.setString(3, entry.pmel_type);
-				ps.setString(4, entry.pmel_line);
+					content.append(Long.toString(pmel_parent));
+				content.append('|');
+
+				escape(entry.pmel_type, content);
+				content.append('|');
+
+				content.append(entry.pmel_line);
+				content.append('|');
+				
+				escape(entry.pmel_content, content);
 				ps.setString(5, entry.pmel_content);
 				
 				// STORE ROW AND GETS INSERTION ID
@@ -76,6 +96,19 @@ public abstract class DbLink implements Callable<Boolean> {
 		queue.offer(entry);
 	}
 	
+	private void escape(CharSequence sgt, StringBuilder append) {		
+		int size = sgt.length();
+		for (int i = 0 ; i < size ; i++) {
+			char character = sgt.charAt(i); 
+			if (character == '\\')
+				append.append(escapeEscape);
+			else if (character == '|')
+				append.append(escapeDelim);
+			else
+				append.append(character);
+		}
+	}
+	
 	protected abstract Long getParent();
 
 	public void setQueue(LinkedBlockingQueue<Entry> queue) {
@@ -86,5 +119,9 @@ public abstract class DbLink implements Callable<Boolean> {
 
 	private static final String query = "INSERT INTO pmel_temp (pmel_root, pmel_parent, pmel_type, pmel_line, pmel_content) "
 			+ "VALUES(?, ?, ?, ?::BIGINT, ?) RETURNING pmel_id";
+
+	private static final char[] escapeEscape = {'\\', '\\'};
+
+	private static final char[] escapeDelim = {'\\', '|'};
 
 }
