eacc8c9f8c008c9453eaaa3fa82000d92138e51c
jean philippe delabre <jeanphilippe.delabre@gmail.com>
2014-06-06 14:31:54 +0200

    Intégration reporting

---
+++ pimsdriver/src/main/java/com/github/aiderpmsi/pimsdriver/jaxrs/Report.java
@@ -0,0 +1,96 @@
+package com.github.aiderpmsi.pimsdriver.jaxrs;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.security.PermitAll;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+
+import com.github.aiderpmsi.pimsdriver.db.DataSourceSingleton;
+
+@Path("/report") 
+@PermitAll
+public class Report {
+	
+	private static final String rootResource = "com/github/aiderpmsi/pimsdriver/jreport/";
+
+	@GET
+    @Path("/report/{id}/factures{finess}.pdf")
+    @Produces({"application/pdf"})
+	public Response getPendingUploadedElements(
+			@QueryParam("id") final Long id,
+			@QueryParam("finess") String finess) {
+		
+		StreamingOutput stream = new StreamingOutput() {
+	
+			@Override
+			public void write(OutputStream os) throws IOException, WebApplicationException {
+			
+				OutputStream bos = new BufferedOutputStream(os);
+				try {
+					JasperReport report = reportFromResourceName("fact_main.jrxml");
+
+					Map<String, Object> parametres = new HashMap<>();
+
+					parametres.put("pmel_root", (Object) id);
+					
+					Connection con = null;
+					try {
+						con = DataSourceSingleton.getInstance().getConnection();
+					
+						JasperPrint print = JasperFillManager.fillReport(report, parametres, con);
+						JasperExportManager.exportReportToPdfStream(print, bos);
+						
+						con.commit();
+					} finally {
+						if (con != null) con.close();
+					}
+				} catch (JRException | SQLException e) {
+					throw new IOException(e);
+				} finally {
+					bos.flush();
+				}
+				return;
+			}
+		};
+		
+		return Response.ok(stream).build();
+	}
+
+	public static JasperReport reportFromResourceName(String resource) throws IOException {
+		InputStream is = null;
+		try {
+			is = new BufferedInputStream(Report.class.getClassLoader().getResourceAsStream(rootResource + resource));
+			JasperDesign design = JRXmlLoader.load(is);
+			JasperReport report = JasperCompileManager.compileReport(design);
+			return report;
+		} catch (JRException e) {
+			throw new IOException(e);
+		} finally {
+			if (is != null) is.close();
+		}
+	}
+	
+}
---
+++ pimsdriver/src/main/java/com/github/aiderpmsi/pimsdriver/vaadin/report/ReportWindow.java
@@ -0,0 +1,40 @@
+package com.github.aiderpmsi.pimsdriver.vaadin.report;
+
+import com.github.aiderpmsi.pimsdriver.dto.model.UploadedPmsi;
+import com.vaadin.server.ExternalResource;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Link;
+import com.vaadin.ui.Window;
+
+public class ReportWindow extends Window {
+
+	/** Serial Id */
+	private static final long serialVersionUID = 5355233025554029932L;
+
+	/** Associated pmsi */
+	private UploadedPmsi pmsi;
+	
+	public ReportWindow(UploadedPmsi pmsi) {
+		// TITLE
+		super("Reporting");
+
+		this.pmsi = pmsi;
+
+		// SET VISUAL ASPECT
+		setWidth("650px");
+		setClosable(true);
+		setResizable(true);
+		setModal(true);
+		setStyleName("reportpmsi");
+		center();
+
+        // SELECT LAYOUT
+        HorizontalLayout hl = new HorizontalLayout();
+        setContent(hl);
+
+		// CREATE LINK TO JAX-RS TO EXPORT ELEMENT
+		Link report = new Link("Rapport", new ExternalResource("../rest/report/" + pmsi.getRecordid() + "/factures" + pmsi.getFiness() + ".pdf"));
+		hl.addComponent(report);
+	}
+
+}
